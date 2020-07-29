d3d6d2bffc0d1a7f890343bbf82e263aaa337abf
Manujaya Premathilaka <manujayapremathilaka@gmail.com>
2020-07-29 17:04:38 +0530

    Working Days and Hours Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,35 @@ import ViewLocation from "./location/ViewLocation";
 import ViewRoomDetails from "./location/ViewRoomDetail";
 import SubjectStat from "./Statistics/SubjectStat";
 import LecturerStats from "./Statistics/LectureStats";
+
+//Working Days
+import WorkingDaysMain from "./WorkingDays/WorkingDaysMain";
+import AddWeekdayWorkingDays from "./WorkingDays/AddWeekdayWorkingDays";
+import AddWeekendWorkingDays from "./WorkingDays/AddWeekendWorkingDays";
+import ViewWeekdayWorkingDays from "./WorkingDays/ViewWeekdayWorkingDays";
+import ViewWeekendWorkingDays from "./WorkingDays/ViewWeekendWorkingDays";
+
+
 function App() {
   return (
       <Router>
         <NavBar/>
         <switch>
 
-          {/*Admin*/}
-          <Route path="/AddLocation" exact component={AddLocationData} />
-          <Route path="/StuStats" component={StudentStats}/>
-          <Route path="/LecturerStats" component={LecturerStats}/>
-          <Route path="/SubjectStats" component={SubjectStat}/>
-          <Route path="/ViewLocation" component={ViewLocation}/>
-          <Route path="/ViewRoom" component={ViewRoomDetails}/>
+            {/*Admin*/}
+            <Route path="/AddLocation" exact component={AddLocationData} />
+            <Route path="/StuStats" component={StudentStats}/>
+            <Route path="/LecturerStats" component={LecturerStats}/>
+            <Route path="/SubjectStats" component={SubjectStat}/>
+            <Route path="/ViewLocation" component={ViewLocation}/>
+            <Route path="/ViewRoom" component={ViewRoomDetails}/>
 
-
+            {/*Working Days*/}
+            <Route path="/WorkingDaysMain" component={WorkingDaysMain}/>
+            <Route path="/AddWeekdayWorkingDays" component={AddWeekdayWorkingDays}/>
+            <Route path="/AddWeekendWorkingDays" component={AddWeekendWorkingDays}/>
+            <Route path="/ViewWeekdayWorkingDays" component={ViewWeekdayWorkingDays}/>
+            <Route path="/ViewWeekendWorkingDays" component={ViewWeekendWorkingDays}/>
         </switch>
       </Router>
   );
---
+++ frontend/src/WorkingDays/ViewWeekendWorkingDays.js
@@ -0,0 +1,56 @@
+import React,{Component} from "react";
+import workingDays from "../assets/css/workingdays.css";
+import WorkingDaysNav from "./Common/ViewWorkingDaysNav";
+
+export default class ViewWeekendWorkingDays extends Component{
+
+    render() {
+        return (
+            <div className="main">
+                <WorkingDaysNav/>
+                <h3>Weekend</h3>
+                <div className="form">
+                    <table style={{width:"100%"}}>
+                        <tr>
+                            <th style={{fontSize:"15px"}}>Total Working Days</th>
+                            <td style={{fontSize:"15px"}}>02</td>
+                        </tr>
+                        <tr>
+                            <th style={{fontSize:"15px"}}>Days</th>
+                            <td style={{fontSize:"15px"}}>
+                                Saturday<br/>
+                                Sunday<br/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style={{fontSize:"15px"}}>Working Hours</th>
+                            <td style={{fontSize:"15px"}}>8 hours 30 minutes</td>
+                        </tr>
+                        <tr>
+                            <th style={{fontSize:"15px"}}>Time Slot</th>
+                            <td style={{fontSize:"15px"}}>One Hour</td>
+                        </tr>
+                    </table>
+                    <br/>
+
+                    <form>
+                        <div className="row">
+                            <div className="column">
+                                <button type="submit"
+                                    className="btn mb-2"
+                                    style={{backgroundColor: "#312450", color: "white", marginLeft: "50%", width: "100px"}}
+                                >Edit</button>
+                            </div>
+                            <div className="column">
+                                <button type="submit"
+                                    className="btn mb-2"
+                                    style={{backgroundColor: "#312450", color: "white", marginLeft: "40px", width: "100px"}}
+                                >Delete</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
